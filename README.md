a0e662581f983fcb1e13f1e481478c461ffa9042
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-08-15 12:50:19 +0100

    Update to navbar

--- README.md
+++
@@ -1,5 +0,0 @@
-# Bindus-Photography
-My First Website
-
-
-https://joe-lindie.github.io/Bindus-Photography/
--- bindus.js
+++ bindus.js
@@ -1,6 +1,18 @@
-const hamburger =  document.getElementById('hamburger');
-const navItem =  document.getElementById('nav-item');
+// const hamburger =  document.getElementById('hamburger');
+// const navItem =  document.getElementById('nav-item');
 
-hamburger.addEventListener('click', () => {
-  navItem.classList.toggle('show');
-});
+// hamburger.addEventListener('click', () => {
+//   navItem.classList.toggle('show');
+// });
+
+const openNavBar = document.getElementById("myNav")
+const closeNavBar = document.getElementById("myNav")
+const hamburger = document.getElementById("openBtn")
+
+closeNavBar.addEventListener("click", function closeNav() {
+  closeNavBar.style.width = "0%"
+})
+
+openBtn.addEventListener("click", function openNav() {
+  openNavBar.style.width = "100%"
+})
