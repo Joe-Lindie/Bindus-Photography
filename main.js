1f361551959bea51106d5da4f644619d42f16663
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-09-03 12:30:18 +0100

    basic slideshow added

--- main.js
+++ main.js
@@ -14,3 +14,39 @@ openMenu.addEventListener("click", function openNav() {
 closeMenu.addEventListener("click", function closeNav() {
   navItems.style.width = "0%"
 })
+
+////////////////////////
+
+// SLIDESHOW
+
+////////////////////////
+
+let i = 0 // Start Point
+const images = [] // Images Array
+const time = 2000 // Time Between Switch
+
+// Image List
+images[0] = "Images/Homepage/Jeju.JPG"
+images[1] = "Images/Homepage/mountain.JPG"
+images[2] = "Images/Homepage/Scotland_1.jpeg"
+images[3] = "Images/Homepage/Scotland_2.jpeg"
+
+function changeImg() {
+  document.slide.src = images[i]
+
+  if (i < images.length - 1) {
+    i++
+  } else {
+    i = 0
+  }
+
+  setTimeout("changeImg()", time)
+}
+
+window.onload = changeImg
+
+////////////////////////
+
+// Next ...
+
+////////////////////////
