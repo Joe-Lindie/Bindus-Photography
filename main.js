d1969902f0e6ca2a720d67a5c1e1f4efee2df8fd
Joe-Lindie <josephlindie@hotmail.co.uk>
2022-09-06 14:46:27 +0100

    images randomly displayed on highlights page

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ closeMenu.addEventListener("click", function closeNav() {
 // SLIDESHOW
 
 ////////////////////////
-
 let i = 0 // Start Point
 const images = [] // Images Array
 const time = 2000 // Time Between Switch
@@ -43,10 +42,53 @@ function changeImg() {
   setTimeout(changeImg, time)
 }
 
-changeImg()
+//changeImg()
+//onload="changeImg()" currently attached to body tag
 
 ////////////////////////
 
-// Next ...
+// DISPLAY IMAGES IN RANDOM ORDER
+
+////////////////////////
+
+const allGalleryImages = document.querySelectorAll(".gallery_img")
+
+let galleryImgs = [
+  "Images/Highlights/BangogjiKorea.jpg",
+  "Images/Highlights/BarcelonaSpain.jpg",
+  "Images/Highlights/Bluelake.jpg",
+  "Images/Highlights/BusanKorea.jpg",
+  "Images/Highlights/Busansunset.jpg",
+  "Images/Highlights/GyeongjuKorea.jpg",
+  "Images/Highlights/HakoneJapan.jpg",
+  "Images/Highlights/Seoul.jpg",
+  "Images/Highlights/Singapore.jpg",
+  "Images/Highlights/Scotland_1.JPG",
+  "Images/Highlights/Scotland_2.JPG",
+  "Images/Highlights/Scotland_3.JPG",
+  "Images/Highlights/Scotland_4.JPG",
+  "Images/Highlights/TokyoJapan.jpeg",
+  "Images/Highlights/CherryBlossoms.JPG",
+  "Images/Highlights/Busan_houseKorea 2.JPG",
+  "Images/Highlights/Deagu.jpg",
+  "Images/Highlights/Japan1.jpg",
+  "Images/Highlights/JejuKorea2.JPG",
+  "Images/Highlights/Gyeongju1.jpg",
+]
+
+function displayInRandomOrder() {
+  //Randomize/Shuffle Array
+  galleryImgs.sort((a, b) => 0.5 - Math.random())
+
+  for (let i = 0; i < allGalleryImages.length; i++) {
+    allGalleryImages[i].src = galleryImgs[i]
+  }
+}
+
+displayInRandomOrder()
+
+////////////////////////
+
+// Next
 
 ////////////////////////
